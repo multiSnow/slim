ce4b8d6a269230abd25563795b5276ddfc2edefc
multiSnow <infinity.blick.winkel@gmail.com>
2017-12-07 23:50:55 +0800

    Fix ordered comparison between pointer and zero

--- panel.cpp
+++ panel.cpp
@@ -48,7 +48,7 @@ Panel::Panel(Display* dpy, int scr, Window root, Cfg* config,
 		gcm = GCGraphicsExposures;
 		gcv.graphics_exposures = False;
 		WinGC = XCreateGC(Dpy, Win, gcm, &gcv);
-		if (WinGC < 0) {
+		if (WinGC < (void*)0) {
 			cerr << APPNAME
 				<< ": failed to create pixmap\n.";
 			exit(ERR_EXIT);
